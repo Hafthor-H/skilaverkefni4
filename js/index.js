b9361708ead5f08586d33842aecae00009927a5e
goobiy <98406264+goobiy@users.noreply.github.com>
2025-05-02 13:49:42 +0000

    óklárað skilaboðasaga
    havtor hugen fokk

--- js/index.js
+++ js/index.js
@@ -5,12 +5,13 @@ import { fileURLToPath } from 'node:url';
 import { Server } from 'socket.io';
 import mongodb from "mongodb";
 
-var i = 0;
 
 const app = express();
 const server = createServer(app)
 const io = new Server(server);
 const { MongoClient } = mongodb;
+
+
 
 var users = [];
 var activeUsers = 0;
@@ -32,16 +33,33 @@ MongoClient.connect("mongodb://127.0.0.1/Skilaverkefni4", { useUnifiedTopology: 
   io.on('connection', (socket) => {
 
 
+
     socket.on("pW", (password) => {
       if (password == "42") {
         socket.emit("lock", true)
         activeUsers++;
-        chatDB.collection("messages").find({}).toArray(function (err, result) {
+        chatDB.collection("messages").find({}, { projection: { _id: 0, message: 1 } }).toArray(function (err, result) {
           if (err) throw err;
           for (let k = 0; k < result.length; k++) {
-            io.emit("chat message", result[k].msg);
+            io.emit("chat message", result[k].message);
           }
         });
+
+        socket.on("usernameHistory", (username) => {
+          console.log("Looking up messages for:", username);  // ekki að virka
+        });
+        // socket.on("usernameHistory", (username) => {
+        //   console.log("Looking up messages for:", username);
+        //   chatDB.collection("messages").find({ user: username }).toArray(function (err, userResult) {
+        //     if (err) {
+        //       throw err;
+        //     }
+        //     for (let j = 0; j < userResult.length; j++) {
+        //       io.emit("chat message", userResult[j].message)
+        //     }
+        //   })
+        // });
+
         io.emit("userNum", activeUsers);
         socket.on("disconnect", () => {
           for (let i = 0; i < users.length; i++) {
@@ -50,22 +68,36 @@ MongoClient.connect("mongodb://127.0.0.1/Skilaverkefni4", { useUnifiedTopology: 
             }
           }
           activeUsers--;
-        })
+        });
         socket.on('chat message', (msg) => {
           io.emit('chat message', Tímiskilaboða() + socket.userName + ": " + msg);
-          chatDB.collection("messages").insertOne({ msg: Tímiskilaboða() + socket.userName + ": " + msg });
+          chatDB.collection("messages").insertOne({ user: socket.userName, message: socket.userName + " : " + msg + " : " + Tímiskilaboða() });
         });
         socket.on("join", (person) => {
           socket.userName = person;
           users.push(socket.userName);
           io.emit("userArray", users);
-        }
-        )
+        });
+        // https://www.w3schools.com/nodejs/nodejs_mongodb_find.asp
+        // socket.on("usernameHistory", (username) => {
+        //   db.getUser(username, { showCustomData: true })
+        //     .then((userData) => {
+        //       if (userData) {
+        //         socket.emit("historyResponse", userData);
+        //       } else {
+        //         socket.emit("historyError", "User not found");
+        //       }
+        //     })
+        //     .catch((error) => {
+        //       console.error("Villa í gagnagrunni: ", error);
+        //       socket.emit("historyError", "Villa í gagnagrunni");
+        //     });
+        // });
       }
       else {
         socket.emit("lock", false)
       }
-    })
+    });
   });
 });
 
